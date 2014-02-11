a23b6f6874bad1950fbbe0e61c43f7141edc4873
cdhanna <cdhanna@wpi.edu>
2014-02-11 01:44:29 -0500

    fixed menu selection spacing bug. Added camera motion for credits button. Theoretically, clicking quit from ingame should cause the game to exit now.

--- Assets/MenuImages/ControllerScript.cs
+++ Assets/MenuImages/ControllerScript.cs
@@ -8,16 +8,45 @@ public class ControllerScript : MonoBehaviour {
 	public GameObject creditsButton;
 	public GameObject quitButton;
 
+	public Transform mainLookAt;
+	public Camera camera;
+
+
 	private GameObject selectedButton;
+
+	private bool inTransitionToCredits = false;
+	private float angleChangeToCredits = 0.0f;
+	private float transSpeed = 10f;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.camera.transform.LookAt (this.mainLookAt.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (this.inTransitionToCredits) {
+			if (this.angleChangeToCredits <110f) {
+				this.camera.transform.Rotate (Vector3.up * transSpeed);
+				this.angleChangeToCredits += transSpeed;
+
+			}
+
+			if (Input.GetKeyDown ("return")) {
+				this.inTransitionToCredits = false;
+				//this.angleChangeToCredits = 110;
+			}
+
+			return;
+		} else {
+			if (this.angleChangeToCredits > 0f) {
+				this.camera.transform.Rotate (Vector3.up * -transSpeed);
+				this.angleChangeToCredits -= transSpeed;
+				
+			}
+		}
+
 		if (Input.GetKeyDown ("down")) {
 			this.selectedButton = this.getNextButton ();
 			this.setSelectionBar ();
@@ -26,10 +55,23 @@ public class ControllerScript : MonoBehaviour {
 			this.setSelectionBar();
 		}
 
+		if (Input.GetKeyDown ("return")) {
+
+			if (this.selectedButton == this.quitButton){
+				Application.Quit();
+			} else if (this.selectedButton == this.creditsButton){
+				this.inTransitionToCredits = true;
+				//this.angleChangeToCredits = 0;
+			}
+
+		}
+
 	}
 
 	private void setSelectionBar(){
-		this.selectorBar.transform.position = new Vector3 (this.selectorBar.transform.position.x, this.selectedButton.transform.position.y, this.selectorBar.transform.position.z);
+		this.selectorBar.transform.position = new Vector3 (this.selectorBar.transform.position.x,
+		                                                   this.playButton.transform.position.y - 1.5f * (this.playButton.transform.position.y - this.selectedButton.transform.position.y),
+		                                                   this.selectorBar.transform.position.z);
 	}
 
 	private GameObject getPrevButton(){
@@ -38,7 +80,7 @@ public class ControllerScript : MonoBehaviour {
 		} else if (this.selectedButton == this.creditsButton) {
 			return this.playButton;
 		} else {
-			return this.quitButton;
+			return this.creditsButton;
 		}
 	}
 
