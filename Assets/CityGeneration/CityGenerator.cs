0277cc146219ebed4a00ad88157657e5ae28b4b7
TCalvert93 <tcalvert93@gmail.com>
2014-02-11 23:58:22 -0500

    Parks can be placed now.
    Parks are placed in random sections. They will currently overwrite
    roads if the width is too wide. They are also always placed from the
    bottom left.
    
    Everything needs to be made more random.

--- Assets/CityGeneration/CityGenerator.cs
+++ Assets/CityGeneration/CityGenerator.cs
@@ -23,9 +23,9 @@ public class CityGenerator : MonoBehaviour {
     // width and height of city
     private int _cityWidth = 64, _cityHeight = 64;
     // width and height (2d) of park
-    private int _parkFootprintWidth = 2, _parkFootprintHeight = 2;
+    private int _parkFootprintWidth = 2, _parkFootprintHeight = 4;  // ** width should probably be 2 as that is the width of buildings in a section
     // number of parks in city
-    private int _numberOfParks = 1;
+    private int _numberOfParks = 2;
 
     /*\
      * Enum for identifying areas
@@ -137,54 +137,15 @@ public class CityGenerator : MonoBehaviour {
             height += subsectionHeight;
         }
 
-        int newEdge;
+        int newEdgeVert, newEdgeHoriz;
+        // Need the new edges of the city so we don't place things outside
+        // The number will be for the outer most road, not the final edge of buildings
+        cityGrid = _cleanUpEdges(cityGrid, subsectionWidth, subsectionHeight, out newEdgeVert, out newEdgeHoriz);
 
-        // Eliminate extras along the edges (either overwrite with buildings, or close them with roads if they are large enough)
-        width = _cityWidth - 2; // _cityWidth - 1 must be a building, this is the first place we can have a road
-        height = 1;  // top layer
-        // Walk back until we find the most recent intersection
-        do {
-            --width;
-        } while (cityGrid[height + 1, width] != FILLTYPE.ROAD);
-        newEdge = width;
-        // This is small, so we really just need to cut off the edges.
-        ++width;
-        for(; width < _cityWidth - 1; ++width) {
-            for (; height < _cityHeight - 1; ++height) {
-                cityGrid[height, width] = FILLTYPE.BULIDING;
-            }
+        // Place parks
+        for (int i = 0; i < _numberOfParks; ++i) {
+            cityGrid = _placePark(cityGrid, newEdgeHoriz, newEdgeVert);
         }
-
-        int newHeight;
-
-        width = 1;
-        height = _cityHeight - 2;
-        do {
-            --height;
-        } while (cityGrid[height, width + 1] != FILLTYPE.ROAD);
-        newHeight = height;
-        Debug.Log("Bottom road at " + height);
-        Debug.Log("Space limit " + (subsectionHeight * 0.75));
-        Debug.Log("Current space " + ((_cityHeight - 2) - height));
-        // Check how large the space we have is left
-        // If it is enough of the subsection, close it off, otherwise eliminate the extra road
-        if ((_cityHeight - 2) - height > subsectionHeight * 0.75) {
-            Debug.Log("Closing road");
-            // Close it off
-            height = _cityHeight - 2;
-            for (; width <= newEdge; ++width) {
-                cityGrid[height, width] = FILLTYPE.ROAD;
-            }
-        } else {
-            Debug.Log("Eliminating road");
-            for (; width <= newEdge; ++width) {
-                for (height = newHeight + 1; height < _cityHeight - 1; ++height) {
-                    cityGrid[height, width] = FILLTYPE.BULIDING;
-                }
-            }
-        }
-
-        // TODO: place parks
 
         // DEBUG
         Texture2D debugTexture = new Texture2D(_cityWidth, _cityHeight);
@@ -210,5 +171,93 @@ public class CityGenerator : MonoBehaviour {
         debugTexture.Apply();
         File.WriteAllBytes(Application.dataPath + "/CityTexture.png", debugTexture.EncodeToPNG());
         Debug.Log("Wrote new file");
+    }
+
+    private FILLTYPE[,] _placePark(FILLTYPE[,] cityGrid, int edgeHoriz, int edgeVert) {
+        // Pick a random point, then walk until it is a building
+        // Then work up and down, left and right replacing building with parks
+        int x, y;
+        x = Random.Range(2, edgeHoriz - 2);  // don't want the outer edge of buildings or the inside ring of 
+        y = Random.Range(2, edgeVert - 2);
+
+        if (cityGrid[y, x] == FILLTYPE.PARK) // just pick another spot
+            return _placePark(cityGrid, edgeHoriz, edgeVert);
+
+        // We should never need more than 3 steps to get off a road
+        if (cityGrid[y, x] == FILLTYPE.ROAD) {
+            ++y;
+            if (cityGrid[y, x] == FILLTYPE.ROAD) {
+                ++x;
+                if (cityGrid[y, x] == FILLTYPE.ROAD) {
+                    ++y;
+                }
+            }
+        }
+
+        // At this point, our point should be within a section of buildings
+        // Now we want to walk down to the bottom left of the section
+        while (cityGrid[y + 1, x] == FILLTYPE.BULIDING) {
+            ++y;
+        }
+        while (cityGrid[y, x - 1] == FILLTYPE.BULIDING) {
+            --x;
+        }
+
+        // Fill in park
+        int tempY = y;
+        for (int i = 0; i < _parkFootprintWidth; ++i) {
+            y = tempY;
+            for (int j = 0; j < _parkFootprintHeight; ++j) {
+                cityGrid[y--, x] = FILLTYPE.PARK;
+            }
+            ++x;
+        }
+
+        return cityGrid;
+    }
+
+    private FILLTYPE[,] _cleanUpEdges(FILLTYPE[,] cityGrid, int subsectionWidth, int subsectionHeight, out int newEdgeVert, out int newEdgeHoriz) {
+        int width, height;
+
+        // Eliminate extras along the edges (either overwrite with buildings, or close them with roads if they are large enough)
+        width = _cityWidth - 2; // _cityWidth - 1 must be a building, this is the first place we can have a road
+        height = 1;  // top layer
+        // Walk back until we find the most recent intersection
+        do {
+            --width;
+        } while (cityGrid[height + 1, width] != FILLTYPE.ROAD);
+        newEdgeHoriz = width;
+        // This is small, so we really just need to cut off the edges.
+        ++width;
+        for (; width < _cityWidth - 1; ++width) {
+            for (; height < _cityHeight - 1; ++height) {
+                cityGrid[height, width] = FILLTYPE.BULIDING;
+            }
+        }
+
+        width = 1;
+        height = _cityHeight - 2;
+        do {
+            --height;
+        } while (cityGrid[height, width + 1] != FILLTYPE.ROAD);
+        newEdgeVert = height;
+        // Check how large the space we have is left
+        // If it is enough of the subsection, close it off, otherwise eliminate the extra road
+        if ((_cityHeight - 2) - height > subsectionHeight * 0.75) {
+            // Close it off
+            height = _cityHeight - 2;
+            for (; width <= newEdgeHoriz; ++width) {
+                cityGrid[height, width] = FILLTYPE.ROAD;
+            }
+        } else {
+            for (; width <= newEdgeHoriz; ++width) {
+                for (height = newEdgeVert + 1; height < _cityHeight - 1; ++height) {
+                    cityGrid[height, width] = FILLTYPE.BULIDING;
+                }
+            }
+        }
+
+        return cityGrid;
+
     }
 }
