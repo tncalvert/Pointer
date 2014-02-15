549fcf2d88d9b642ba60f61f3e4cfe4d503c0fea
TCalvert93 <tcalvert93@gmail.com>
2014-02-15 17:07:06 -0500

    Beginning of steering forces.
    Added two base scripts for steering. SteeringForce is an interface
    that all steering behaviors must implement. SteeringObject is a
    script to be placed on any object that we want steering behaviors
    applied to. Also added a small scene to do testing in. It will be
    removed once steering is finalized.

---
+++ Assets/Scripts/Steering/SteeringObject.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Allows the object containing this script to be moved by a series of steering forces
+/// Requires that the object has a rigid body on it
+/// Class based on demonstrations and code by Prof. Kesselman
+/// </summary>
+public class SteeringObject : MonoBehaviour {
+
+    /// <summary>
+    /// The maximum forces that the steering behaviors can exert on the object
+    /// </summary>
+    public float maxForce = 1.0f;
+
+    /// <summary>
+    /// The maximum velocity that results from the application of a steering force
+    /// </summary>
+    public float maxVelocity = 3.0f;
+
+    /// <summary>
+    /// A list of all steering forces acting on this object
+    /// </summary>
+    List<SteeringForce> steeringForces = new List<SteeringForce>();
+
+    /// <summary>
+    /// A flag that indicates the object has a rigidbody. Update is aborted if this is set to false
+    /// </summary>
+    private bool hasRigidbody = false;
+
+	void Start () { 
+        // Check to see if we have a rigid body
+        try {
+            GetComponent<Rigidbody>();
+            hasRigidbody = true;
+        } catch (MissingComponentException e) {
+            Debug.Log("Object " + this.name + " does not have a Rigidbody.\n" + e.Message);
+        }
+    }
+	
+	void Update () {
+
+        if (!rigidbody) {
+            // If there is no rigidbody, abort
+            return;
+        }
+
+        Vector3 force = new Vector3(0, 0, 0);
+
+        // Accumulate the effects of all steering forces
+        foreach (SteeringForce sf in steeringForces) {
+            force = force + sf.GetSteeringForce(maxVelocity);
+        }
+
+        // Limit the force
+        force = Vector3.ClampMagnitude(force, maxForce);
+
+        // Apply the force
+        GetComponent<Rigidbody>().AddForce(force);
+	}
+
+    /// <summary>
+    /// Adds a new steering force to the object
+    /// Does not check if there is already a steering force of this type on the object
+    /// </summary>
+    /// <param name="sf">The new steering force to add</param>
+    public void AddSteeringForce(SteeringForce sf) {
+        steeringForces.Add(sf);
+    }
+
+    /// <summary>
+    /// Removes a steering force from the object
+    /// Does not handle duplicates
+    /// </summary>
+    /// <param name="sf">Steering force to remove</param>
+    public void RemoveSteeringForce(SteeringForce sf) {
+        steeringForces.Remove(sf);
+    }
+}
