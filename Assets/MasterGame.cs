b23931d43b89e2d1f2f8c506ae1377ad1381e745
TCalvert93 <tcalvert93@gmail.com>
2014-02-23 16:53:31 -0500

    Change to player attack and splatter.
    Splatter now works when killing victims, and is sized appriately
    (though I personally liked the city sized blood splatters).
    Additionally, moved player attack code out of the steering
    code into its own script.

--- Assets/MasterGame.cs
+++ Assets/MasterGame.cs
@@ -7,7 +7,6 @@ public class MasterGame : MonoBehaviour {
 	public BuildingGenerator buildingGenerator;
 	public CityGenerator cityGenerator;
 	public PathFinder pathFinder;
-	public SplatterScript splatter;
 
 	//Uhm, I dont know what I'm doing anymore
 	public GameObject follower;
@@ -115,7 +114,6 @@ public class MasterGame : MonoBehaviour {
 	//I am teh unity n00b and I don't know if this is a good way to go about generating people
 	private PlayerSteering generateFollower(Vector2 position){
 		PlayerSteering f = ((GameObject)Instantiate (this.follower, new Vector3(position.x, 1, position.y), Quaternion.identity)).GetComponent<PlayerSteering>();
-		f.splatter = this.splatter;
 		return f;
 	}
 
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    //// <summary>
+    /// The splatter. Squishy.
+    /// </summary>
+    public SplatterScript splatter;
+    public GameObject splatterPrefab;
+
+    /// <summary>
+    /// Attack range for the monster
+    /// </summary>
+    int attackRange = 5;
+
+    /// <summary>
+    /// Player score
+    /// </summary>
+    int playerScore = 0;
+
+    void Start() {
+        splatter = gameObject.AddComponent<SplatterScript>();
+        splatter.bloodSplat = splatterPrefab;
+    }
+
+	void Update () {
+        //Kill the victims
+        if (Input.GetKeyDown("space")) {
+            Collider[] nearbyVictims = Physics.OverlapSphere(rigidbody.position, attackRange, 1 << LayerMask.NameToLayer("Victims"));
+
+            foreach (var n in nearbyVictims) {
+                if (this.splatter != null) {
+                    this.splatter.makeSplat(n.transform.position, new Vector3(0, -1, 0), 10, (1 << LayerMask.NameToLayer("City")));
+                }
+
+                Destroy(n.gameObject);
+                playerScore++;
+
+                Debug.Log("Score: " + playerScore);
+            }
+
+        }
+	}
+}
