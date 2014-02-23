17a8a2e41e797e2949601127f8e8eb1c22b60b94
cdhanna <cdhanna@wpi.edu>
2014-02-23 16:54:32 -0500

    sample fuzz logic

--- Assets/Scripts/FuzzyLogic/SampleFuzz.cs
+++ Assets/Scripts/FuzzyLogic/SampleFuzz.cs
@@ -5,10 +5,32 @@ public class SampleFuzz : MonoBehaviour {
 
 	FuzzyBrain fBrain;
 
-	FuzzyBrain.FuzzySet set;
+	FuzzyBrain.FuzzySet actionSet;
+
+	public float task(FuzzyBrain.FuzzySet set){
+		return 0;
+	}
 
 	public SampleFuzz(){
 		fBrain = new FuzzyBrain();
+
+		actionSet = new FuzzyBrain.FuzzySet ();
+
+		actionSet.doOperation (task);
+
+		//
+		actionSet.setElement ("afraid", .4f);
+		actionSet.setElement ("brave", .2f);
+
+
+
+		actionSet.normalize ();
+		if (actionSet.getFuzzyMax ().Equals ("afraid")) {
+			//RUN!!!
+		} else {
+			//FIGHT!!!!!!!!
+		}
+
 	}
 
 	// Use this for initialization
