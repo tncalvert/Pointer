eaeaf2ec203983e9070cb549dda84d37e9c95a93
TCalvert93 <tcalvert93@gmail.com>
2014-02-26 16:25:56 -0500

    Beginning of genetic algorithm.
    Added some initial files for genetic algorith. Class descriptions
    should give all the info needed, but in short VictimMonitor tracks
    any values we need to determine fitness, and GeneticMaster will
    handle applying the actual genetic algorithm.

---
+++ Assets/Scripts/Genetic/GeneticMaster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Main class in genetic algorithm.
+/// Will handle figuring out success of victims, and building new generations
+/// from the results of the previous level.
+/// 
+/// --NOTES--
+/// This will work slightly differently from a standard genetic algorithm
+/// We will generate a random population, which will serve as the victims
+/// of the first level. After the level is over, victims are evaulated on
+/// various criteria:
+///     - Survival time         (30%)
+///     - Damage done to player (70%)
+/// After the level is completed. We will take a subset of the population
+/// (50%??) and use them to create a new generation of vicitims.
+/// However, we do preserve the other section of the list that has been
+/// determined as failures. After running the next level, we will then
+/// take the next section from the combination of the victims played
+/// in the level, and the older, failed, victims to make sure we aren't
+/// introducing failing victims into the game.
+/// </summary>
+public class GeneticMaster : MonoBehaviour {
+
+    /// <summary>
+    /// Class to hold a victim and its corresponding values, success, etc.
+    /// </summary>
+    private class GeneticVictim {
+
+    }
+
+    List<GeneticVictim> victims;
+
+    void Start() {
+        victims = new List<GeneticVictim>();
+    }
+
+
+}
---
+++ Assets/Scripts/Genetic/VictimMonitor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class that monitors the various factors that contribute
+/// to the fitness of a victim.
+/// </summary>
+public class VictimMonitor : MonoBehaviour {
+
+    /// <summary>
+    /// The survival time of the victim, measured since the level loaded
+    /// </summary>
+    public float timeSpentAlive;
+
+    /// <summary>
+    /// The amount of damage dealt to the player
+    /// </summary>
+    public float damageToPlayer;
+
+    /// <summary>
+    /// Time marked at the start of the level
+    /// </summary>
+    private float startTime;
+
+    void Start() {
+        startTime = Time.realtimeSinceStartup;
+        damageToPlayer = 0;
+    }
+
+    /// <summary>
+    /// Function called when the victim dies. Sets the time the victim survived.
+    /// </summary>
+    void OnDestroy() {
+        timeSpentAlive = Time.realtimeSinceStartup - startTime;
+    }
+
+    /// <summary>
+    /// Called when this victim deals damage to the player
+    /// </summary>
+    /// <param name="damageAmount">The amount of damage done</param>
+    void DealtDamage(float damageAmount) {
+        damageToPlayer += damageAmount;
+    }
+}
