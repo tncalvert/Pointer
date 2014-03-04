931bc245a32231a94662c63fff1ed6815b3a3989
TCalvert93 <tcalvert93@gmail.com>
2014-03-03 22:12:47 -0500

    Mutations and some minor changes.
    Fixed possibility for a victim to have a single parent (weird).
    Changed ToString method of GeneticVictim to fit the correct
    order of listed attributes.
    
    Added in mutations. With a 50% chance, a victim is selected to be
    mutated. Then we randomly select between 1 and 3 attributes to mutate.
    How an attribute is mutated depends somewhat on which one it is
    (see code for details). Each one is then clamped between 0 and some
    upper bound (again, depends on the attribute).

--- Assets/Scripts/Genetic/GeneticMaster.cs
+++ Assets/Scripts/Genetic/GeneticMaster.cs
@@ -80,8 +80,8 @@ public class GeneticMaster : MonoBehaviour {
             temp += "\tBravery: " + data.AttribBravery;
             temp += "\tToughness: " + data.AttribIndependence;
             temp += "\tIndependence: " + data.AttribToughness + "\n";
-            temp += "\tMax Velocity: " + data.MaxVelocity;
-            temp += "\tMax Force: " + data.MaxForce + "\n";
+            temp += "\tMax Force: " + data.MaxForce;
+            temp += "\tMax Velocity: " + data.MaxVelocity + "\n";
             temp += "\tArrival: " + data.MinimumArrivalRadiusSqrd;
             temp += "\tComplete Arrival: " + data.MinimumCompleteArrivalRadiusSqrd;
             temp += "\tUnique Path: " + data.UniquePathProbability;
@@ -93,9 +93,9 @@ public class GeneticMaster : MonoBehaviour {
             temp += "\t\tFear: " + data.SteeringFear;
             temp += "\tSeek: " + data.SteeringSeek;
             temp += "\tSeparation: " + data.SteeringSeparation + "\n";
+            temp += "\tSidewalk: " + data.SteeringSideWalkLove;
             temp += "\t\tWall Avoidance: " + data.SteeringWallAvoidance;
-            temp += "\tWander: " + data.SteeringWander;
-            temp += "\tSidewalk: " + data.SteeringSideWalkLove + "\n\n";
+            temp += "\tWander: " + data.SteeringWander + "\n\n";
 
             return temp;
         }
@@ -157,6 +157,24 @@ public class GeneticMaster : MonoBehaviour {
     /// An integer that will be appended to the log file name and incremented every level
     /// </summary>
     private static uint runCount;
+
+    // Default ranges for values. They will be changed after some training.
+    float[] attribRange = { 0.3f, 0.7f };   // Range for bravery, toughness and independence
+    float[] maxV = { 10f, 30f };        // max velocity
+    float[] maxF = { 20f, 60f };        // max force
+    float[] minArriv = { 4f, 16f };    // minimum arrival radius sqrd
+    float[] minComp = { 2f, 9f };     // minimum complete arrival radius sqrd
+    float[] uniqP = { 0.2f, 0.8f };       // unique path probability
+    float[] pathCR = { 6f, 14f };      // path check radius
+    float[] sAlign = { 0.75f, 1.25f };      // steering alignment
+    float[] sCohes = { 0.75f, 1.25f };      // steering cohesion
+    float[] sCollAvoid = { 2f, 4f };  // collision avoidance
+    float[] sFear = { 3f, 5f };       // fear
+    float[] sSeek = { 0.75f, 1.25f };       // seek
+    float[] sSepar = { 1f, 1.4f };      // separation
+    float[] sWallAvoid = { 2.25f, 3.25f };  // wall avoidance
+    float[] sWand = { 0.3f, 0.9f };       // wander
+    float[] sSWL = { 0.75f, 1.25f };        // sidewalk love
 
     void Start() {
 
@@ -267,7 +285,6 @@ public class GeneticMaster : MonoBehaviour {
         int sizeDiff = victims.Count - halfSize;
 
         // Combine victims and discardedVictims and sort them
-        Debug.Log(discardedVictims);
         List<GeneticVictim> sortedVictims = victims.Concat(discardedVictims).ToList();
         sortedVictims = sortedVictims.OrderByDescending(m => m.fitness).ToList();
 
@@ -296,9 +313,12 @@ public class GeneticMaster : MonoBehaviour {
 
         // Here we'll build all the new victims
         for (int i = 0; i < sizeDiff; ++i) {
-            // Pick two random victims to breed
+            // Pick two random victims to breed, make sure we get different parents
             GeneticVictim vic1 = victims[weights.randomIndex()];
-            GeneticVictim vic2 = victims[weights.randomIndex()];
+            GeneticVictim vic2;
+            do {
+                vic2 = victims[weights.randomIndex()];
+            } while (vic2 == vic1);
 
             GameObject g = (GameObject)Instantiate(victimPrefab,
                 new Vector3(initialPositions[streetIdx].x, 1, initialPositions[streetIdx].y), Quaternion.identity);
@@ -311,47 +331,33 @@ public class GeneticMaster : MonoBehaviour {
             VictimData vd = g.GetComponent<VictimData>();
             VictimSteering vs = g.GetComponent<VictimSteering>();
 
-            int rand = Random.Range(0, 2);
-            vd.AttribBravery = (rand == 0 ? vic1.data.AttribBravery : vic2.data.AttribBravery);
-            rand = Random.Range(0, 2);
-            vd.AttribIndependence = (rand == 0 ? vic1.data.AttribIndependence : vic2.data.AttribIndependence);
-            rand = Random.Range(0, 2);
-            vd.AttribToughness = (rand == 0 ? vic1.data.AttribToughness : vic2.data.AttribToughness);
+            vd.AttribBravery = (Random.Range(0, 2) == 0 ? vic1.data.AttribBravery : vic2.data.AttribBravery);
+            vd.AttribIndependence = (Random.Range(0, 2) == 0 ? vic1.data.AttribIndependence : vic2.data.AttribIndependence);
+            vd.AttribToughness = (Random.Range(0, 2) == 0 ? vic1.data.AttribToughness : vic2.data.AttribToughness);
 
-            rand = Random.Range(0, 2);
-            vd.MaxForce = (rand == 0 ? vic1.data.MaxForce : vic2.data.MaxForce);
-            rand = Random.Range(0, 2);
-            vd.MaxVelocity = (rand == 0 ? vic1.data.MaxVelocity : vic2.data.MaxVelocity);
-            rand = Random.Range(0, 2);
-            vd.MinimumArrivalRadiusSqrd = (rand == 0 ? vic1.data.MinimumArrivalRadiusSqrd : vic2.data.MinimumArrivalRadiusSqrd);
-            rand = Random.Range(0, 2);
-            vd.MinimumCompleteArrivalRadiusSqrd = (rand == 0 ? vic1.data.MinimumCompleteArrivalRadiusSqrd : vic2.data.MinimumCompleteArrivalRadiusSqrd);
-            rand = Random.Range(0, 2);
-            vd.UniquePathProbability = (rand == 0 ? vic1.data.UniquePathProbability : vic2.data.UniquePathProbability);
-            rand = Random.Range(0, 2);
-            vd.PathCheckRadius = (rand == 0 ? vic1.data.PathCheckRadius : vic2.data.PathCheckRadius);
+            vd.MaxForce = (Random.Range(0, 2) == 0 ? vic1.data.MaxForce : vic2.data.MaxForce);
+            vd.MaxVelocity = (Random.Range(0, 2) == 0 ? vic1.data.MaxVelocity : vic2.data.MaxVelocity);
+            vd.MinimumArrivalRadiusSqrd = (Random.Range(0, 2) == 0 ? vic1.data.MinimumArrivalRadiusSqrd : vic2.data.MinimumArrivalRadiusSqrd);
+            vd.MinimumCompleteArrivalRadiusSqrd = (Random.Range(0, 2) == 0 ? vic1.data.MinimumCompleteArrivalRadiusSqrd : vic2.data.MinimumCompleteArrivalRadiusSqrd);
+            vd.UniquePathProbability = (Random.Range(0, 2) == 0 ? vic1.data.UniquePathProbability : vic2.data.UniquePathProbability);
+            vd.PathCheckRadius = (Random.Range(0, 2) == 0 ? vic1.data.PathCheckRadius : vic2.data.PathCheckRadius);
 
-            rand = Random.Range(0, 2);
-            vd.SteeringAlignment = (rand == 0 ? vic1.data.SteeringAlignment : vic2.data.SteeringAlignment);
-            rand = Random.Range(0, 2);
-            vd.SteeringCohesion = (rand == 0 ? vic1.data.SteeringCohesion : vic2.data.SteeringCohesion);
-            rand = Random.Range(0, 2);
-            vd.SteeringCollisionAvoidance = (rand == 0 ? vic1.data.SteeringCollisionAvoidance : vic2.data.SteeringCollisionAvoidance);
-            rand = Random.Range(0, 2);
-            vd.SteeringFear = (rand == 0 ? vic1.data.SteeringFear : vic2.data.SteeringFear);
-            rand = Random.Range(0, 2);
-            vd.SteeringSeek = (rand == 0 ? vic1.data.SteeringSeek : vic2.data.SteeringSeek);
-            rand = Random.Range(0, 2);
-            vd.SteeringSeparation = (rand == 0 ? vic1.data.SteeringSeparation : vic2.data.SteeringSeparation);
-            rand = Random.Range(0, 2);
-            vd.SteeringSideWalkLove = (rand == 0 ? vic1.data.SteeringSideWalkLove : vic2.data.SteeringSideWalkLove);
-            rand = Random.Range(0, 2);
-            vd.SteeringWallAvoidance = (rand == 0 ? vic1.data.SteeringWallAvoidance : vic2.data.SteeringWallAvoidance);
-            rand = Random.Range(0, 2);
-            vd.SteeringWander = (rand == 0 ? vic1.data.SteeringWander : vic2.data.SteeringWander);
+            vd.SteeringAlignment = (Random.Range(0, 2) == 0 ? vic1.data.SteeringAlignment : vic2.data.SteeringAlignment);
+            vd.SteeringCohesion = (Random.Range(0, 2) == 0 ? vic1.data.SteeringCohesion : vic2.data.SteeringCohesion);
+            vd.SteeringCollisionAvoidance = (Random.Range(0, 2) == 0 ? vic1.data.SteeringCollisionAvoidance : vic2.data.SteeringCollisionAvoidance);
+            vd.SteeringFear = (Random.Range(0, 2) == 0 ? vic1.data.SteeringFear : vic2.data.SteeringFear);
+            vd.SteeringSeek = (Random.Range(0, 2) == 0 ? vic1.data.SteeringSeek : vic2.data.SteeringSeek);
+            vd.SteeringSeparation = (Random.Range(0, 2) == 0 ? vic1.data.SteeringSeparation : vic2.data.SteeringSeparation);
+            vd.SteeringSideWalkLove = (Random.Range(0, 2) == 0 ? vic1.data.SteeringSideWalkLove : vic2.data.SteeringSideWalkLove);
+            vd.SteeringWallAvoidance = (Random.Range(0, 2) == 0 ? vic1.data.SteeringWallAvoidance : vic2.data.SteeringWallAvoidance);
+            vd.SteeringWander = (Random.Range(0, 2) == 0 ? vic1.data.SteeringWander : vic2.data.SteeringWander);
 
-            // TODO: MUTATIONS!!!!
+            // With a 50% probability, mutate some data
+            if (Random.Range(0f, 1f) <= 0.5f) {
+                mutateData(vd);
+            }
 
+            // Set up links and data for the victim
             v.victimID = victimIds++;
             v.data = vd;
             v.monitor = vm;
@@ -408,28 +414,112 @@ public class GeneticMaster : MonoBehaviour {
     }
 
     /// <summary>
+    /// Mutates some of the data in a victim randomly.
+    /// </summary>
+    /// <param name="vd">The VictimData to mutate. It's a reference so there's no need to return anything</param>
+    private void mutateData(VictimData vd) {
+
+        // Pick the number of mutations to do
+        int numOfMutations = Random.Range(1, 4); // 1, 2 or 3 values can be mutated
+
+        // Copy all the values into an array so we can pick random indexes
+        float[] valArray = 
+        {
+            vd.AttribBravery, vd.AttribIndependence, vd.AttribToughness,
+            vd.MaxForce, vd.MaxVelocity, vd.MinimumArrivalRadiusSqrd,
+            vd.MinimumCompleteArrivalRadiusSqrd, vd.PathCheckRadius,
+            vd.UniquePathProbability, vd.SteeringAlignment, vd.SteeringCohesion,
+            vd.SteeringCollisionAvoidance, vd.SteeringFear, vd.SteeringSeek,
+            vd.SteeringSeparation, vd.SteeringSideWalkLove,
+            vd.SteeringWallAvoidance, vd.SteeringWander
+        };
+
+        // Loop through and mutate items
+        for (int i = 0; i < numOfMutations; ++i) {
+            int mutIdx = Random.Range(0, valArray.Length);  // Last value is exlusive, so we won't ever get valArray.length
+
+            float mutAmount;
+
+            // Get a mutation value
+            // Since these are floats, we can't use xor without a lot of hacky things that might cause more problems
+            // than they solve, so we'll pick a random value to either add or subtract based on which value
+            // we're mutating
+            if (mutIdx <= 2) { // Attributes
+                mutAmount = Random.Range(-0.4f, 0.4f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;  // Don't want to actually hit 0
+                } else if (valArray[mutIdx] - mutAmount >= 1f) {
+                    mutAmount = -(1f - valArray[mutIdx] - 0.00001f);
+                }
+            } else if (mutIdx > 2 && mutIdx <= 4) {  // Max velocity/force
+                mutAmount = Random.Range(-20f, 20f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;
+                } else if (valArray[mutIdx] - mutAmount >= 100f) {
+                    mutAmount = -(100f - valArray[mutIdx] - 0.00001f);
+                }
+            } else if (mutIdx > 4 && mutIdx <= 6) {  // Min arrival *
+                mutAmount = Random.Range(-4f, 4f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;
+                } else if (valArray[mutIdx] - mutAmount >= 25f) {
+                    mutAmount = -(25f - valArray[mutIdx] - 0.00001f);
+                }
+            } else if (mutIdx == 8) {  // Unique path prob
+                mutAmount = Random.Range(-0.6f, 0.6f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;
+                } else if (valArray[mutIdx] - mutAmount >= 1f) {
+                    mutAmount = -(1f - valArray[mutIdx] - 0.00001f);
+                }
+            } else if (mutIdx == 7) {  // Path radius
+                mutAmount = Random.Range(-8f, 8f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;
+                } else if (valArray[mutIdx] - mutAmount >= 20f) {
+                    mutAmount = -(20f - valArray[mutIdx] - 0.00001f);
+                }
+            } else {  // Steering
+                mutAmount = Random.Range(-2.5f, 2.5f);
+                if (valArray[mutIdx] - mutAmount <= 0f) {
+                    mutAmount = valArray[mutIdx] + 0.00001f;
+                } else if (valArray[mutIdx] - mutAmount >= 8f) {
+                    mutAmount = -(8f - valArray[mutIdx] - 0.00001f);
+                }
+            }
+
+            valArray[mutIdx] -= mutAmount;
+            
+        }
+
+        // Restore the values
+        vd.AttribBravery = valArray[0];
+        vd.AttribIndependence = valArray[1];
+        vd.AttribToughness = valArray[2];
+
+        vd.MaxForce = valArray[3];
+        vd.MaxVelocity = valArray[4];
+        vd.MinimumArrivalRadiusSqrd = valArray[5];
+        vd.MinimumCompleteArrivalRadiusSqrd = valArray[6];
+        vd.PathCheckRadius = valArray[7];
+        vd.UniquePathProbability = valArray[8];
+
+        vd.SteeringAlignment = valArray[9];
+        vd.SteeringCohesion = valArray[10];
+        vd.SteeringCollisionAvoidance = valArray[11];
+        vd.SteeringFear = valArray[12];
+        vd.SteeringSeek = valArray[13];
+        vd.SteeringSeparation = valArray[14];
+        vd.SteeringSideWalkLove = valArray[15];
+        vd.SteeringWallAvoidance = valArray[16];
+        vd.SteeringWander = valArray[17];
+
+    }
+
+    /// <summary>
     /// Generates the initial population on the first time the game loads
     /// </summary>
     public void generateInitialPopulation() {
-
-        // Default ranges for these values. They will be changed after some training.
-        float[] attribRange = { 0.3f, 0.7f };   // Range for bravery, toughness and independence
-        float[] maxV = { 10f, 30f };        // max velocity
-        float[] maxF = { 20f, 60f };        // max force
-        float[] minArriv = { 4f, 16f };    // minimum arrival radius sqrd
-        float[] minComp = { 2f, 9f };     // minimum complete arrival radius sqrd
-        float[] uniqP = { 0.2f, 0.8f };       // unique path probability
-        float[] pathCR = { 6f, 14f };      // path check radius
-        float[] sAlign = { 0.75f, 1.25f };      // steering alignment
-        float[] sCohes = { 0.75f, 1.25f };      // steering cohesion
-        float[] sCollAvoid = { 2f, 4f };  // collision avoidance
-        float[] sFear = { 3f, 5f };       // fear
-        float[] sSeek = { 0.75f, 1.25f };       // seek
-        float[] sSepar = { 1f, 1.4f };      // separation
-        float[] sWallAvoid = { 2.25f, 3.25f };  // wall avoidance
-        float[] sWand = { 0.3f, 0.9f };       // wander
-        float[] sSWL = { 0.75f, 1.25f };        // sidewalk love
-
 
         foreach (Vector2 p in initialPositions) {
             GameObject g = (GameObject)Instantiate(victimPrefab, new Vector3(p.x, 1, p.y), Quaternion.identity);
