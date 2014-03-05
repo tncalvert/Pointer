598544ff60d04e65c1316e13c19f9d5103925864
rmsantos <rmsantos@wpi.edu>
2014-03-05 10:07:14 -0500

    Added visual attack timer

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -20,21 +20,42 @@ public class PlayerAttack : MonoBehaviour {
 	//Wilhelm Scream
 	AudioClip scream;
 
+	//Attack Tracker
+	AttackTracker attack;
+
     void Start() {
         splatter = gameObject.AddComponent<SplatterScript>();
         splatter.bloodSplat = splatterPrefab;
+
+		//Keep track of victims
+		GameObject attackObj = GameObject.Find ("AttackTimer");
+		attack = attackObj.GetComponent<AttackTracker> ();
 	}
 
 	void Update () {
 		if(attackTimer != 120)
 		{
 			attackTimer++;
+
+			if(attackTimer == 30)
+				attack.setText("3");
+
+			if(attackTimer == 60)
+				attack.setText("2");
+
+			if(attackTimer == 90)
+				attack.setText("1");
+
 			if(attackTimer == 120)
-				Debug.Log ("Attack Ready");
+				attack.setText ("Attack Ready!");
 		}
 
         //Kill the victims
         if (attackTimer == 120 && Input.GetKeyDown("space")) {
+			
+			attackTimer = 0;
+			attack.setText("4");
+
             Collider[] nearbyVictims = Physics.OverlapSphere(rigidbody.position, attackRange, 1 << LayerMask.NameToLayer("Victims"));
 
             foreach (var n in nearbyVictims) {
@@ -47,8 +68,6 @@ public class PlayerAttack : MonoBehaviour {
 
 			if(nearbyVictims.Length != 0)
 				audio.Play ();
-
-			attackTimer = 0;
         }
 	}
 }
