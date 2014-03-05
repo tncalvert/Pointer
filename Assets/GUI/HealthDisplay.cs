f0dbaffd511f51a397c93ccfe39504062a801249
rmsantos <rmsantos@wpi.edu>
2014-03-05 09:58:38 -0500

    Added health tracker

---
+++ Assets/GUI/HealthDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthDisplay : MonoBehaviour {
+	
+	public float health;
+	
+	// Use this for initialization
+	void Start () {
+		health = 9;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "Health: " + health;
+	}
+	
+	//Increments the score
+	public void doDamage(float damage)
+	{
+		health -= damage;
+	}
+}
